910a8ed8560af007fe529f0c0a563323fa2d999a
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-15 22:25:47 +0530

    Implemented the no-args constructor for Academic class, 'identity' and 'university' values set to null

--- src/Entities/Academic.java
+++ src/Entities/Academic.java
@@ -4,4 +4,10 @@ public class Academic extends Person {
     private String identity;
 
     private String university;
+
+    public Academic() {
+        super();
+        this.identity = null;
+        this.university = null;
+    }
 }
