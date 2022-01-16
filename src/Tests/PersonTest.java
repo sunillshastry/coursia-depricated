fbfa95d0ebe8edcdb9ef319b4bf29511b43a2f5c
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-16 21:20:09 +0530

    Added a 'public' keyword to each test case in the Person class for better understanding

--- src/Tests/PersonTest.java
+++ src/Tests/PersonTest.java
@@ -8,33 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonTest {
 
     @Test
-    void getName() {
+    public void getName() {
         Person person = new Person("Rick Hoffman");
         assertEquals("Rick Hoffman", person.getName());
     }
 
     @Test
-    void getAge() {
+    public void getAge() {
         Person person = new Person(20);
         assertEquals(20, person.getAge());
     }
 
     @Test
-    void getNameAndAge() {
+    public void getNameAndAge() {
         Person person = new Person("Mark Snow", 55);
         assertEquals("Mark Snow", person.getName());
         assertEquals(55, person.getAge());
     }
 
     @Test
-    void setAndGetName() {
+    public void setAndGetName() {
         Person person = new Person();
         person.setName("Holly Winks");
         assertEquals("Holly Winks", person.getName());
     }
 
     @Test
-    void setAndGetAge() {
+    public void setAndGetAge() {
         Person person = new Person();
         person.setAge(69);
         assertEquals(69, person.getAge());
