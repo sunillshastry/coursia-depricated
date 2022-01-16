9c115a598bb3a06ef4e599708a2aafb3634b7ba0
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-16 20:41:53 +0530

    Added a test case for Academic class - a no args constructor testing for 'null' name value

--- src/Tests/AcademicTest.java
+++ src/Tests/AcademicTest.java
@@ -1,8 +1,14 @@
 package Tests;
 
 import org.junit.jupiter.api.Test;
+import Entities.Academic;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AcademicTest {
+    @Test
+    void NoArgsAcademicName() {
+        Academic academic = new Academic();
+        assertNull(academic.getName());
+    }
 }
